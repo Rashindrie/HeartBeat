1eec1b54486e189c656448de7df69ba5f69bd7ec
Rashindrie Perera <rashindrie.14@cse.mrt.ac.lk>
2017-05-16 16:55:32 +0530

    Migration Removed

--- db/migrate/20170501001927_remove_columns.rb
+++ db/migrate/20170501001927_remove_columns.rb
@@ -1,12 +1,5 @@
 class RemoveColumns < ActiveRecord::Migration[5.0]
   def change
-    remove_column("time_slots", "amount")
-
-    remove_column("appointments", "notification_params")
-    remove_column("appointments", "payment_status")
-    remove_column("appointments", "transaction_id")
-    remove_column("appointments", "purchased_at")
-
 
   end
 end
